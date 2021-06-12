4beaaf40c3a0dbfe72838d4fca1716d5bfec5b95
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-06-12 23:21:12 +0900

    Initial Commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by 深根幹貴 on 2021/06/12.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
